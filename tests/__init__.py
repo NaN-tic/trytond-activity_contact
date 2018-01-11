689ff985e8f43bc085ccd01e9a06a1bc174f96c5
C?dric Krier <ced@b2ck.com>
2018-01-11 12:22:27 +0100

    Update to 4.7

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_activity_contact import suite
+try:
+    from trytond.modules.activity_contact.tests.test_activity_contact import suite
+except ImportError:
+    from .test_activity_contact import suite
+
+__all__ = ['suite']
