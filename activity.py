069a1f8b84334c9bddc39caacb05b5b5e0ab59da
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2025-09-17 15:54:00 +0200

    Implemented sequence contacts for activities Also dinamically updates allowed_contacts on party change, without needing to save the activity first.
    Task #174614

--- activity.py
+++ activity.py
@@ -1,25 +1,26 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelSQL, fields
+from trytond.model import ModelSQL, ModelView, fields, sequence_ordered
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
+from trytond.transaction import Transaction
 
 __all__ = ['ActivityParty', 'Activity']
 
 
-class ActivityParty(ModelSQL, metaclass=PoolMeta):
+class ActivityParty(sequence_ordered(), ModelView, ModelSQL, metaclass=PoolMeta):
     'Activity'
     __name__ = "activity.activity-party.party"
 
     activity = fields.Many2One('activity.activity', 'Activity',
         required=True, ondelete='CASCADE')
-    party = fields.Many2One('party.party', 'Party', required=True)
-
-
-class Activity(metaclass=PoolMeta):
-    'Activity'
-    __name__ = "activity.activity"
-
+    party = fields.Many2One('party.party', 'Party',
+            domain=[('id', 'in', Eval('allowed_contacts', [])),],
+        context={
+                'company': Eval('company', -1),
+            },
+        depends=['allowed_contacts', 'company'],
+        required=True)
     allowed_contacts = fields.Function(fields.Many2Many('party.party',
             None, None, 'Allowed Contacts',
             context={
@@ -27,23 +28,28 @@ class Activity(metaclass=PoolMeta):
             },
             depends=['company']),
         'on_change_with_allowed_contacts')
-    contacts = fields.Many2Many('activity.activity-party.party', 'activity',
-        'party', 'Contacts', domain=[
-            ('id', 'in', Eval('allowed_contacts', [])),
-            ],
-        context={
-            'company': Eval('company', -1),
-            },
-        depends=['allowed_contacts', 'company'])
+    company = fields.Many2One('company.company', "Company", required=True)
 
-    @fields.depends('party', methods=['on_change_with_party'])
+
+    @fields.depends('_parent_activity.id', '_parent_activity.party', 'activity',
+                     'company','party')
     def on_change_with_allowed_contacts(self, name=None):
         pool = Pool()
         Employee = pool.get('company.employee')
-
-        res = [e.party.id for e in Employee.search([])]
-        self.party = self.on_change_with_party()
-        if not self.party or self.party.id < 0:
-            return res
-        res.extend(r.to.id for r in self.party.relations)
+        res = []
+        if self.activity:
+            res = [e.party.id for e in Employee.search(
+                ['company', '=', self.activity.company.id])]
+            if self.activity.party:
+                res.extend(r.to.id for r in self.activity.party.relations)
         return res
+
+    @classmethod
+    def default_company(cls):
+        return Transaction().context.get('company')
+
+class Activity(metaclass=PoolMeta):
+    'Activity'
+    __name__ = "activity.activity"
+
+    contacts = fields.One2Many('activity.activity-party.party', 'activity', 'Contacts',)
